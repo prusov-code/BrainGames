d42015c05618a8f8d82c2b02c7e9fe9a320dc689
prusov-code <prusov-official@yandex.ru>
2025-02-01 23:13:17 +0300

    Refactoring source code for PSR12

--- src/Cli.php
+++ src/Cli.php
@@ -1,10 +1,13 @@
 <?php
+
 namespace Brain\Games\Cli;
+
 use function cli\line;
 use function cli\prompt;
+
 function welcomeUser()
 {
     line('Welcome to the Brain Game!');
-    $name=prompt('May I have your name?');
-    line("Hello, %s!",$name);
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
