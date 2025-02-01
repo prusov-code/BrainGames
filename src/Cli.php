36778651b9c26ebb4a69f2f814018f8d362f5efa
prusov-code <prusov-official@yandex.ru>
2025-02-02 00:33:18 +0300

    Update Cli.php, added return value (user name)

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,10 @@ namespace Brain\Games\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function welcomeUser()
+function welcomeUser(): string
 {
     line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    $userName = prompt('May I have your name?');
+    line("Hello, %s!", $userName);
+    return $userName;
 }
