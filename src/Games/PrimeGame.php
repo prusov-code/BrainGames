9bc8135e09074efdf43f7ede50dd6ca212a264ce
prusov-code <prusov-official@yandex.ru>
2025-02-02 05:28:41 +0300

    Refactoring of src/Games/PrimeGame.php

--- src/Games/PrimeGame.php
+++ src/Games/PrimeGame.php
@@ -6,6 +6,20 @@ use function cli\line;
 use function cli\prompt;
 use function BrainGames\Engine\gameEngine;
 
+function isPrime($number): bool
+{
+    if ($number == 1) {
+        return 0;
+    } else {
+        for ($i = 2; $i <= sqrt($number); $i++) {
+            if ($number % $i == 0) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
 function primeGame(): void
 {
     $gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
@@ -13,18 +27,7 @@ function primeGame(): void
     $gameAnswers = [];
     for ($i = 0; $i < 3; $i++) {
         $randomNumber = rand(1, 100);
-        $correctAnswer = 'yes';
-        //Checking is number prime
-        if ($randomNumber == 1) {
-            $correctAnswer = 'no';
-        } else {
-            for ($x = 2; $x <= sqrt($randomNumber); $x++) {
-                if ($randomNumber % $x == 0) {
-                    $correctAnswer = 'no';
-                }
-            }
-        }
-        $gameAnswers[] = $correctAnswer;
+        $gameAnswers[] = isPrime($randomNumber) ? 'yes' : 'no';
         $gameQuestions[] = $randomNumber;
     }
     gameEngine($gameDescription, $gameQuestions, $gameAnswers);
